e0328e5dcac83313314f94809555ceb2401c1811
Antoine Cotten <tonio.cotten@gmail.com>
2017-06-23 10:02:03 +0200

    Update base image to Alpine Linux 3.6

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+alpine=3.6
+
 versions=( */ )
 versions=( "${versions[@]%/}" )
 
@@ -12,7 +14,7 @@ for version in "${versions[@]}"; do
 	packageVersions=()
 	for repo in main community; do
 		packageVersions+=(
-			$(curl -sS http://dl-cdn.alpinelinux.org/alpine/v3.5/$repo/x86_64/APKINDEX.tar.gz |
+			$(curl -sS http://dl-cdn.alpinelinux.org/alpine/v"$alpine"/"$repo"/x86_64/APKINDEX.tar.gz |
 				tar -xzO APKINDEX |
 				( grep transmission-daemon -A 12 || : ) |
 				awk -F':' '$1=="V" {print $2; exit}'
@@ -23,6 +25,7 @@ for version in "${versions[@]}"; do
 	fullVersion="$(printf "%s\n" "${packageVersions[@]}" | sort -V | tail -1)"
 
 	sed -i \
+		-e 's/^\(FROM alpine:\).*/\1'"$alpine"'/' \
 		-e 's/^\(ENV TD_VERSION\) .*/\1 '"$fullVersion"'/' \
 		"$version/Dockerfile"
 done
